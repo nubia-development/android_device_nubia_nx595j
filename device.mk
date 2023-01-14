6d33a1805ac285a9b240b5d97bed001302118668
Cyborg2017 <wangyang0987654@gmail.com>
2023-01-14 23:21:49 +0800

    nx595j: Build custom fingerprint hidl service
    Change-Id: Ibdd2e6d21e416f78435fed2371348f71f4cd8a3e

--- device.mk
+++ device.mk
@@ -46,6 +46,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/display/qdcm_calib_data_jdi_nt35596s_1080x2040_mipi_video_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_jdi_nt35596s_1080x2040_mipi_video_panel.xml
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.nx595j \
+    android.hardware.biometrics.fingerprint@2.1.vendor
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/nubia_synaptics_dsx.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/nubia_synaptics_dsx.kl
